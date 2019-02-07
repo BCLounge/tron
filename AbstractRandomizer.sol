58cee2015cae2c72e0be232728e48e9cb6ad1f91
1099511627776 <1099511627776@mail.ru>
2019-02-07 19:21:09 +0200

    add check ticket function

--- AbstractRandomizer.sol
+++ AbstractRandomizer.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.4.23;
 contract AbstractRandomizer {
     function random(uint256 seed) public view returns (uint256, uint256) {
         uint blockNumber = block.number;
-        bytes32 blockHashPrevious = blockhash(blockNumber - 1);
-        bytes32 _seed0 = keccak256(abi.encodePacked(blockHashPrevious, seed));
-        bytes32 _seed1 = keccak256(abi.encodePacked(_seed0,block.timestamp));
-        uint256 _seed = uint256(_seed1);
-        return (_seed % seed + 1, blockNumber);
+        bytes memory _preseed = abi.encodePacked(seed, blockNumber);
+        bytes32 _seed0 = keccak256(_preseed);
+        uint256 _seed = uint256(_seed0);
+        return ( (_seed % seed) + 1, blockNumber);
     }//random
+
+    function checkRandom(uint256 seed, uint blockNumber) public pure returns (uint256){
+        bytes memory _preseed = abi.encodePacked(seed, blockNumber);
+        bytes32 _seed0 = keccak256(_preseed);
+        uint256 _seed = uint256(_seed0);
+        return (_seed % seed) + 1;
+    }
 }
